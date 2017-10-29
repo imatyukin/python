9edf66d856b457c6309076d7254164529750943e
igor <i.matukin@gmail.com>
2017-10-29 14:24:09 +0300

    Did chapter 16 exercise 8.

---
+++ PythonCrashCourse/chapter_16/exercise16_08.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+import unittest
+
+from my_country_codes import get_country_code
+
+class CountryCodesTestCase(unittest.TestCase):
+    """Tests for country_codes.py."""
+
+    def test_get_country_code(self):
+        country_code = get_country_code('Andorra')
+        self.assertEqual(country_code, 'ad')
+
+        country_code = get_country_code('United Arab Emirates')
+        self.assertEqual(country_code, 'ae')
+
+        country_code = get_country_code('Afghanistan')
+        self.assertEqual(country_code, 'af')
+
+unittest.main()
