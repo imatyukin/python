cb88f93463559752a6f76e1a24584e549b57fb81
igor <i.matukin@gmail.com>
2017-11-03 00:09:19 +0300

    Junos PyEZ

---
+++ JunosPyEZ/connect/connect.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+from jnpr.junos import Device
+from getpass import getpass
+import sys
+
+hostname = input("Device hostname: ")
+username = input("Device username: ")
+password = getpass("Device password: ")
+
+# NETCONF session over SSH
+dev = Device(host=hostname, user=username, passwd=password)
+
+try:
+    dev.open()
+except Exception as err:
+    print (err)
+    sys.exit(1)
+
+print (dev.connected)
+dev.close()
+
+print (dev.connected)
