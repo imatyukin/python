921a4c7ed548325e63f60a6e8beae8ea6e50700d
igor <i.matukin@gmail.com>
2017-11-17 23:00:16 +0300

    learning_log

--- PythonCrashCourse/learning_log/virtualenv.sh
+++ PythonCrashCourse/learning_log/virtualenv.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env sh
 
+# Установка virtualenv
+# pip3 install --user virtualenv
+
 # Создание виртуальной среды
 # python -m venv ll_env
 
 # Активизация виртуальной среды
 source ll_env/bin/activate
+
+# Завершить использование виртуальной среды
+# deactivate
