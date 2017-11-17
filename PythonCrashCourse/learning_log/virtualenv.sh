41c5d28c30d0af2be671b682b5c50eda1827bcac
igor <i.matukin@gmail.com>
2017-11-17 23:24:33 +0300

    learning_log

--- PythonCrashCourse/learning_log/virtualenv.sh
+++ PythonCrashCourse/learning_log/virtualenv.sh
@@ -11,3 +11,18 @@ source ll_env/bin/activate
 
 # Завершить использование виртуальной среды
 # deactivate
+
+# Установка Django
+# pip3 install Django
+
+# Создание проекта в Django
+# django-admin.py startproject learning_log .
+
+# Создание базы данных
+# python3 manage.py migrate
+
+# Просмотр проекта
+python3 manage.py runserver
+
+# Начало работы над приложением
+# python3 manage.py startapp learning_logs
