6ff8ecb45a85fbded7aec528a28a33e9931ae055
igor <i.matukin@gmail.com>
2017-11-29 00:41:30 +0300

    Junos PyEZ

---
+++ JunosPyEZ/network_testing/network-testing.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+from jnpr.junos import Device
+from os.path import split, splitext, isfile, join
+from os import listdir
+
+script_dir = split(__file__)[0] or "."
+for f in listdir(script_dir):
+    if isfile(join(script_dir, f)) and f.startswith("tests_") and f.endswith(".py"):
+        exec("from %s import *" % splitext(f)[0])
+
+HOSTS = {
+    "R1": "10.251.78.135",
+    "R2": "10.251.78.137",
+    "R3": "10.251.78.138",
+}
+
+USER = "lab"
+PASSWD = "lab123"
+
+if __name__ == "__main__":
+
+    tests_success = 0
+    tests_fail = 0
+
+    for host in sorted(HOSTS.keys()):
+        print("Running tests for %s" % host)
+        with Device(host=HOSTS[host], user=USER, passwd=PASSWD, gather_facts=False) as dev:
+            for name in dir():
+                if name.startswith("test_%s_" % host) or name.startswith("test_all_"):
+                    print("    Running %s... " % name, end="")
+                    test_result = locals()[name](dev)
+                    if test_result:
+                        print(" pass")
+                        tests_success += 1
+                    else:
+                        print(" ***FAIL***")
+                        tests_fail += 1
+
+    print("--------")
+    print("Network test script finished. Successful tests: %s, failed tests: %s" %
+          (tests_success, tests_fail))
+    print("All went OK." if tests_fail == 0 else "***WARNING***: There were failed tests!")
---
+++ JunosPyEZ/network_testing/tests_memory.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+import jxmlease
+
+def test_all_total_memory_percent_util(dev):
+    parser = jxmlease.EtreeParser()
+    res = parser(dev.rpc.get_route_engine_information())
+    # The 80% threshold is arbitrary and you might want to set different number in your env.
+    # Also consider checking control and data plane memory utilization separately
+    return (int(res["route-engine-information"]["route-engine"][0]["memory-buffer-utilization"]) < 80)
