3ec2c2f933d7b259ba981a1bdd352cecf14527ab
igor <i.matukin@gmail.com>
2017-12-04 00:03:21 +0300

    learning_log

--- PythonCrashCourse/learning_log/learning_logs/views.py
+++ PythonCrashCourse/learning_log/learning_logs/views.py
@@ -52,7 +52,7 @@ def new_topic(request):
 @login_required
 def new_entry(request, topic_id):
     """Добавляет новую запись по конкретной теме."""
-    topic = Topic.objects.get(id=topic_id)
+    topic = get_object_or_404(Topic, id=topic_id)
     check_topic_owner(topic.owner, request.user)
 
     if request.method != 'POST':
@@ -74,7 +74,7 @@ def new_entry(request, topic_id):
 @login_required
 def edit_entry(request, entry_id):
     """Редактирует существующую запись."""
-    entry = Entry.objects.get(id=entry_id)
+    entry = get_object_or_404(Entry, id=entry_id)
     topic = entry.topic
     check_topic_owner(topic.owner, request.user)
 
