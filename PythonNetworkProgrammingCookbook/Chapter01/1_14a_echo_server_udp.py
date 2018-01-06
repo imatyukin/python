2a935c0922353f6f75bc36f4c610681e22f1b07a
igor <i.matukin@gmail.com>
2018-01-07 02:33:53 +0300

    Python Network Programming Cookbook, Second Edition

--- PythonNetworkProgrammingCookbook/Chapter01/1_14a_echo_server_udp.py
+++ PythonNetworkProgrammingCookbook/Chapter01/1_14a_echo_server_udp.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # Python Network Programming Cookbook, Second Edition -- Chapter - 1
 # This program is optimized for Python 2.7.12 and Python 3.5.2.
 # It may run on any other version with/without modifications.
