417d0edc411cd66f34cbc8a6428e1dab8b1ba762
igor <i.matukin@gmail.com>
2018-01-06 23:45:12 +0300

    Python Network Programming Cookbook, Second Edition

--- PythonNetworkProgrammingCookbook/Chapter01/1_1_local_machine_info.py
+++ PythonNetworkProgrammingCookbook/Chapter01/1_1_local_machine_info.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # Python Network Programming Cookbook, Second Edition -- Chapter - 1
 # This program is optimized for Python 2.7.12 and Python 3.5.2.
 # It may run on any other version with/without modifications.
@@ -8,6 +8,7 @@ import socket
 
 def print_machine_info():
     host_name = socket.gethostname()
+    # echo 127.0.0.1 $HOSTNAME >> /etc/hosts or socket.gethostbyname('localhost')
     ip_address = socket.gethostbyname(host_name)
     print ("Host name: %s" %host_name)
     print ("IP address: %s" %ip_address)
