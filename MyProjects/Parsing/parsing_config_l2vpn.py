5d01cfa104c97954f2de18517ff20fbe9007aade
igor <i.matukin@gmail.com>
2018-01-22 00:32:56 +0300

    Parsing

--- MyProjects/Parsing/parsing_config_l2vpn.py
+++ MyProjects/Parsing/parsing_config_l2vpn.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
-# Программа берёт unit'ы интерфейса связанные с encapsulation ccc из файла units
-# и ищет строки, связанные с этими unit'ами, в файле interface, содержащем все настройки интерфейса.
+# Программа берёт подинтерфейсы связанные с encapsulation ccc из файла units
+# и ищет строки связанные с ними в файле interface, содержащем все настройки интерфейса.
 from functools import reduce
 
 # вывод команды: show configuration interfaces xe-2/3/0 | match ccc | display set
@@ -8,15 +8,15 @@ units = open('units', 'r').read()
 # вывод команды: show configuration interfaces xe-2/3/0 | display set
 interface = open('interface', 'r').read().splitlines()
 
-# Находит пятое поле (номер подинтерфейса)
+# Находит пятое поле (номер unit'а)
 units = [fields.split()[4] for fields in units.splitlines()]
-# Сортирует по значению (в арифметическом порядке)
+# Сортирует элемены списка в арифметическом порядке
 units = [int(x) for x in units]
 units.sort()
 # Находит уникальные элементы в списке
 unique_units = reduce(lambda l, x: l+[x] if x not in l else l, units, [])
 
-# Находит в конфигурации интерфейса строки содержащие номер подинтерфейса
+# Находит в конфигурации интерфейса строки содержащие номер unit'а
 for line in interface:
     for unit in unique_units:
         if ' ' + str(unit) + ' ' in line:
