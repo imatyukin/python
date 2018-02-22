d372944ec61ae6d59b6d536412cdbe2cc5a2f998
igor <i.matukin@gmail.com>
2018-02-23 01:31:28 +0300

    Parsing

--- MyProjects/Parsing/parsing_conf_l2circuit.py
+++ MyProjects/Parsing/parsing_conf_l2circuit.py
@@ -33,9 +33,9 @@ with open(target_router_conf, 'w') as target_router_conf:
     router_conf_line = router_conf.splitlines()
 
     # Объявление регулярных выражений и переменных
-    ifl_regex = re.compile('ae6.\w+')
-    ifd_source = 'ae6'
-    ifd_target = 'ae9'
+    ifl_regex = re.compile('xe-2/3/0.\w+')
+    ifd_source = 'xe-2/3/0'
+    ifd_target = 'xe-9/3/3'
     ip_source_neighbor = '87.226.134.133'
     ip_target_neighbor = '95.167.88.60'
     # Интерфейсы, которые переносить не нужно
