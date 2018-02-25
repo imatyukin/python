30ddca8397081fbde9cd0b6384436f6258661b7e
igor <i.matukin@gmail.com>
2018-02-25 15:38:56 +0300

    chapter03

--- AutomateTheBoringStuffWithPython/chapter03/the_collatz_sequence.py
+++ AutomateTheBoringStuffWithPython/chapter03/the_collatz_sequence.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
 
+
 result_collatz = 0
+
 
 def main():
 
     global result_collatz
 
-    while (result_collatz != 1):
+    while result_collatz != 1:
         print("Введите целое число: ")
         try:
             number = int(input())
@@ -18,11 +20,13 @@ def main():
 
     print("Возвращено значение 1.")
 
+
 def collatz(number):
     if number % 2 == 0:
         return number // 2
     elif number % 2 == 1:
         return 3 * number + 1
+
 
 if __name__ == "__main__":
     main()
