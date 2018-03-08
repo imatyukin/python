d68050c362a23f853a5de9834e6375df2581a48f
igor <i.matukin@gmail.com>
2018-03-08 10:08:12 +0300

    chapter05

--- AutomateTheBoringStuffWithPython/chapter05/fantasy_game_inventory.py
+++ AutomateTheBoringStuffWithPython/chapter05/fantasy_game_inventory.py
@@ -4,8 +4,7 @@
 def main():
     stuff = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}
 
-    item_total = displayInventory(stuff)
-    print("Total number of items: " + str(item_total))
+    displayInventory(stuff)
 
 
 def displayInventory(inventory):
@@ -14,7 +13,7 @@ def displayInventory(inventory):
     for k, v in inventory.items():
         print(v, k)
         item_total += v
-    return item_total
+    print("Total number of items: " + str(item_total))
 
 
 if __name__ == "__main__":
