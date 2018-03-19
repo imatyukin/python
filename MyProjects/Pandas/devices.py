1a25b434e4b925272c30e340f600f95f20dbafb1
igor <i.matukin@gmail.com>
2018-03-19 20:13:28 +0300

    Pandas

--- MyProjects/Pandas/devices.py
+++ MyProjects/Pandas/devices.py
@@ -4,6 +4,9 @@ import pandas as pd
 df = pd.read_csv('devices.csv', sep=';', encoding='utf-8')
 
 header = ["Hostname", "IP-address"]
-df.to_csv('as8997.csv', columns=header, index=False, sep='\t', encoding='utf-8')
+df.to_csv('as8997.dat', columns=header, index=False, sep='\t', encoding='utf-8')
 
-print(df.loc[:,['Hostname','IP-address']])
+# print(df.loc[:,['Hostname','IP-address']])
+
+data = pd.read_table('as8997.dat', index_col='Hostname')
+print(data[:])
