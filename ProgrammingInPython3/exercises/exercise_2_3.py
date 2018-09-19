e8e874b8343eed14e7ba9938d6aa667917720b31
igor <i.matukin@gmail.com>
2018-09-19 22:25:40 +0300

    Exercises

---
+++ ProgrammingInPython3/exercises/exercise_2_3.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python3
+# 3. Delete the escape_html() function from csv2html.py, and use the xml.sax.
+#    saxutils.escape() function from the xml.sax.saxutils module instead. This
+#    is easy, requiring one new line (the import), five deleted lines (the unwanted
+#    function), and one changed line (to use xml.sax.saxutils.escape() instead
+#    of escape_html()). A solution is provided in csv2html1_ans.py.
+
+import sys
+import xml.sax.saxutils
+
+def main():
+    maxwidth = 100
+    print_start()
+    count = 0
+    while True:
+        try:
+            line = input()
+            if count == 0:
+                color = "lightgreen"
+            elif count % 2:
+                color = "white"
+            else:
+                color = "lightyellow"
+            print_line(line, color, maxwidth)
+            count += 1
+        except EOFError:
+            break
+    print_end()
+
+
+def print_start():
+    print("<table border='1'>")
+
+
+def print_line(line, color, maxwidth):
+    print("<tr bgcolor='{0}'>".format(color))
+    fields = extract_fields(line)
+    for field in fields:
+        if not field:
+            print("<td></td>")
+        else:
+            number = field.replace(",", "")
+            try:
+                x = float(number)
+                print("<td align='right'>{0:d}</td>".format(round(x)))
+            except ValueError:
+                field = field.title()
+                field = field.replace(" And ", " and ")
+                if len(field) <= maxwidth:
+                    field = xml.sax.saxutils.escape(field)
+                else:
+                    field = "{0} ...".format(
+                            xml.sax.saxutils.escape(field[:maxwidth]))
+                print("<td>{0}</td>".format(field))
+    print("</tr>")
+
+
+def extract_fields(line):
+    fields = []
+    field = ""
+    quote = None
+    for c in line:
+        if c in "\"'":
+            if quote is None: # start of quoted string
+                quote = c
+            elif quote == c:  # end of quoted string
+                quote = None
+            else:
+                field += c    # other quote inside quoted string
+            continue
+        if quote is None and c == ",": # end of a field
+            fields.append(field)
+            field = ""
+        else:
+            field += c        # accumulating a field
+    if field:
+        fields.append(field)  # adding the last field
+    return fields
+
+
+def print_end():
+    print("</table>")
+
+
+main()
