06f8ba1064e867a18252d7ef472819a362283c8b
igor <i.matukin@gmail.com>
2018-09-24 23:02:08 +0300

    Exercises

--- ProgrammingInPython3/exercises/exercise_2_4.py
+++ ProgrammingInPython3/exercises/exercise_2_4.py
@@ -5,6 +5,7 @@
 #    process_options() is called it should set a default maxwidth of 100, and a
 #    default format of “.0f”—this will be used as the format specifier when outputting
 #    numbers.
+#
 #    If the user has typed “-h” or “--help” on the command line, a usage message
 #    should be output and (None, None) returned. (In this case main() should
 #    do nothing.) Otherwise, the function should read any command-line
@@ -34,40 +35,43 @@
 #    nine for the usage message). This exercise may prove challenging for inexperienced
 #    programmers.
 #
-#    Two files of test data are provided: data/co2-sample.csv and data/co2-fromfossilfuels.
+#    Two files of test data are provided: data/co2-sample.csv and data/co2-from-fossilfuels.
 #    csv. A solution is provided in csv2html2_ans.py. In Chapter 5
 #    we will see how to use Python’s optparse module to simplify command-line
 #    processing.
 
 import sys
+import xml.sax.saxutils
 
 
 def main():
-    maxwidth = 100
-    print_start()
-    count = 0
-    while True:
-        try:
-            line = input()
-            if count == 0:
-                color = "lightgreen"
-            elif count % 2:
-                color = "white"
-            else:
-                color = "lightyellow"
-            print_line(line, color, maxwidth)
-            count += 1
-        except EOFError:
-            break
-    print_end()
+    maxwidth, format = process_options()
+    if maxwidth is not None:
+        print_start()
+        count = 0
+        while True:
+            try:
+                line = input()
+                if count == 0:
+                    color = "lightgreen"
+                elif count % 2:
+                    color = "white"
+                else:
+                    color = "lightyellow"
+                print_line(line, color, maxwidth, format)
+                count += 1
+            except EOFError:
+                break
+        print_end()
 
 
 def print_start():
     print("<table border='1'>")
 
 
-def print_line(line, color, maxwidth):
+def print_line(line, color, maxwidth, format):
     print("<tr bgcolor='{0}'>".format(color))
+    numberFormat = "<td align='right'>{{0:{0}}}</td>".format(format)
     fields = extract_fields(line)
     for field in fields:
         if not field:
@@ -76,15 +80,15 @@ def print_line(line, color, maxwidth):
             number = field.replace(",", "")
             try:
                 x = float(number)
-                print("<td align='right'>{0:d}</td>".format(round(x)))
+                print(numberFormat.format(x))
             except ValueError:
                 field = field.title()
                 field = field.replace(" And ", " and ")
                 if len(field) <= maxwidth:
-                    field = escape_html(field)
+                    field = xml.sax.saxutils.escape(field)
                 else:
                     field = "{0} ...".format(
-                            escape_html(field[:maxwidth]))
+                            xml.sax.saxutils.escape(field[:maxwidth]))
                 print("<td>{0}</td>".format(field))
     print("</tr>")
 
@@ -121,6 +125,38 @@ def escape_html(text):
 
 def print_end():
     print("</table>")
+
+
+def process_options():
+    maxwidth_arg = 'maxwidth='
+    format_arg = 'format='
+    maxwidth = 100
+    format = ".0f"
+    for arg in sys.argv[1:]:
+        if arg in ("-h", "--help"):
+            print('''usage:
+csv2html.py [maxwidth=int] [format=str] < infile.csv > outfile.html
+
+maxwidth is an optional integer; if specified, it sets the maximum
+number of characters that can be output for string fields,
+otherwise a default of 100 characters is used.
+(maxwidth - необязательное целое число. Если задано, определяет
+максимальное число символов для строковых полей. В противном случае
+используется значение по умолчанию 100.)
+
+format is the format to use for numbers; if not specified it
+defaults to ".0f".
+(format - формат вывода чисел. Если не задан, по умолчанию используется
+формат ".0f".)''')
+            return None, None
+        elif arg.startswith(maxwidth_arg):
+            try:
+                maxwidth = int(arg.split('=')[1])
+            except ValueError:
+                pass
+        elif arg.startswith(format_arg):
+            format = str(arg.split('=')[1])
+    return maxwidth, format
 
 
 main()
