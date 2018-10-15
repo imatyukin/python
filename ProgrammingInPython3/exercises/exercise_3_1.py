109c0c63695a7c0ee14063d674f0e89b894376b5
igor <i.matukin@gmail.com>
2018-10-15 23:37:16 +0300

    Exercises

---
+++ ProgrammingInPython3/exercises/exercise_3_1.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# 1. Modify the external_sites.py program to use a default dictionary. This is
+#    an easy change requiring an additional import, and changes to just two
+#    other lines. A solution is provided in external_sites_ans.py.
+
+import sys
+import collections
+
+
+sites = collections.defaultdict(set)
+for filename in sys.argv[1:]:
+    with open(filename) as file:
+        for line in file:
+            i = 0
+            while True:
+                site = None
+                i = line.find("http://", i)
+                if i > -1:
+                    i += len("http://")
+                    for j in range(i, len(line)):
+                        if not (line[j].isalnum() or line[j] in ".-"):
+                            site = line[i:j].lower()
+                            break
+                    if site and "." in site:
+                        sites[site].add(filename)
+                    i = j
+                else:
+                    break
+
+for site in sorted(sites):
+    print("{0} is referred to in:".format(site))
+    for filename in sorted(sites[site], key=str.lower):
+        print("    {0}".format(filename))
+
