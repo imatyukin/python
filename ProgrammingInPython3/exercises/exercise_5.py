901a8113be0ee2024e405b8777e2152899765113
igor <i.matukin@gmail.com>
2018-12-01 22:40:05 +0300

    Exercises

--- ProgrammingInPython3/exercises/exercise_5.py
+++ ProgrammingInPython3/exercises/exercise_5.py
@@ -83,47 +83,58 @@ def file_processing(files, dirs, top, modified, order, sizes):
         print(line)
 
 
-def filter_walk(paths, hidden=False, modified=False, order='name', recursive=False, sizes=False):
+def filter_walk(top, hidden=False, modified=False, order='name', recursive=False, sizes=False):
     dirs_count = 0
     files_count = 0
-    if recursive is False:
-        files = []
-        dirs = []
-        for path in paths:
+    files = []
+    dirs = []
+    for entry in top:
+        if recursive is False:
             if hidden is False:
-                for entry in os.listdir(path):
-                    if os.path.isfile(entry):
-                        if not entry.startswith("."):
-                            files.append(entry)
-                            files_count += 1
-                    else:
-                        if not entry.startswith("."):
-                            dirs.append(entry)
-                            dirs_count += 1
-            else:
-                for entry in os.listdir(path):
-                    if os.path.isfile(entry):
+                if os.path.isfile(entry):
+                    if not entry.startswith("."):
                         files.append(entry)
                         files_count += 1
-                    else:
-                        dirs.append(entry)
-                        dirs_count += 1
-            file_processing(files, dirs, paths, modified, order, sizes)
+                elif os.path.isdir(entry):
+                    for name in os.listdir(entry):
+                        if not name.startswith("."):
+                            fullname = os.path.join(entry, name)
+                            if os.path.isfile(fullname):
+                                files.append(fullname)
+                                files_count += 1
+                            elif os.path.isdir(fullname):
+                                dirs.append(fullname)
+                                dirs_count +=1
+                else:
+                    print(f'Файл {entry} не найден.')
+            else:
+                if os.path.isfile(entry):
+                    files.append(entry)
+                    files_count += 1
+                else:
+                    for item in os.listdir(entry):
+                        if os.path.isfile(item):
+                            files.append(item)
+                            files_count += 1
+                        else:
+                            dirs.append(item)
+                            dirs_count += 1
+            file_processing(files, dirs, top, modified, order, sizes)
             print(f'\n{files_count} files, {dirs_count} directories')
-    else:
-        for path, dirs, files in os.walk(paths):
-            if hidden is False:
-                dirs[:] = [dir for dir in dirs if not dir.startswith(".")]
-            real_path = os.path.abspath(os.path.realpath(path))
-            print('Directory: {:s}'.format(real_path))
-            dirs_count += 1
-            for file in files:
-                fstat = os.stat(os.path.join(path, file))
-                fsize = fstat.st_size
-                mtime = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime(fstat.st_mtime))
-                print('{:18s}\t{:10d}\t{:15.15s}'.format(mtime, fsize, file))
-                files_count += 1
-        print('\n{} files, {} directories'.format(files_count, dirs_count))
+        else:
+            for path, dirs, files in os.walk(top):
+                if hidden is False:
+                    dirs[:] = [dir for dir in dirs if not dir.startswith(".")]
+                real_path = os.path.abspath(os.path.realpath(path))
+                print('Directory: {:s}'.format(real_path))
+                dirs_count += 1
+                for file in files:
+                    fstat = os.stat(os.path.join(path, file))
+                    fsize = fstat.st_size
+                    mtime = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime(fstat.st_mtime))
+                    print('{:18s}\t{:10d}\t{:15.15s}'.format(mtime, fsize, file))
+                    files_count += 1
+            print('\n{} files, {} directories'.format(files_count, dirs_count))
 
 
 def parser_arguments():
@@ -150,15 +161,15 @@ The paths are optional; if not given . is used."""
                         action="store_true",
                         help="show sizes [default: off]")
     parser.set_defaults(order=orderlist[0])
-    args, paths = parser.parse_known_args()
-    if not paths:
-        paths = ["."]
-    return args, paths
+    args, top = parser.parse_known_args()
+    if not top:
+        top = ["."]
+    return args, top
 
 
 def main():
-    opts, paths = parser_arguments()
-    filter_walk(paths=paths, hidden=opts.hidden, modified=opts.modified, order=opts.order,
+    opts, top = parser_arguments()
+    filter_walk(top=top, hidden=opts.hidden, modified=opts.modified, order=opts.order,
                 recursive=opts.recursive, sizes=opts.sizes)
 
 
