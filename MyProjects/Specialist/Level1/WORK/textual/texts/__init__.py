a62de395636bb870df8c99aad26c275aef5612be
igor <i.matukin@gmail.com>
2018-12-02 14:23:41 +0300

    Specialist Python Level 1

--- MyProjects/Specialist/Level1/WORK/textual/texts/__init__.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-from .words import words_from_file
-from .analyze_words import *
-from .out import *
--- MyProjects/Specialist/Level1/WORK/textual/texts/words.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-
-def words_from_file(source):
-    for line in source:
-        wrd = line.split()
-        for x in wrd:
-            yield x
