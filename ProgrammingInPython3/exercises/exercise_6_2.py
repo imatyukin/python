19d3f1ecc54ffd33484d3791dc115c9c7d9e75bb
igor <i.matukin@gmail.com>
2018-12-17 23:16:10 +0300

    Exercises

--- ProgrammingInPython3/exercises/exercise_6_2.py
+++ ProgrammingInPython3/exercises/exercise_6_2.py
@@ -352,6 +352,23 @@ class Image:
         >>> image.width, image.height, len(image.colors)
         (5, 5, 2)
         """
+        if width is None and height is None:
+            return False
+        if width is None:
+            width = self.width
+        if height is None:
+            height = self.height
+        if width >= self.width and height >= self.height:
+            self.__width = width
+            self.__height = height
+            return True
+        self.__width = width
+        self.__height = height
+        for x, y in list(self.__data.keys()):
+            if x >= self.width or y >= self.height:
+                del self.__data[(x, y)]
+        self.__colors = set(self.__data.values()) | {self.__background}
+        return True
 
 
 if __name__ == "__main__":
