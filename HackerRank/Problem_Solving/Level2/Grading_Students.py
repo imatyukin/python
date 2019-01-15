660b8119493d210fb5453c68d8d63d2325dbcc12
igor <i.matukin@gmail.com>
2019-01-15 23:17:30 +0300

    HackerRank

---
+++ HackerRank/Problem_Solving/Level2/Grading_Students.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+Sample Input 0
+4
+73
+67
+38
+33
+Sample Output 0
+75
+67
+40
+33
+Explanation 0
+
+
+Student  received a , and the next multiple of  from  is . Since , the student's grade is rounded to .
+Student  received a , and the next multiple of  from  is . Since , the grade will not be modified and the student's final grade is .
+Student  received a , and the next multiple of  from  is . Since , the student's grade will be rounded to .
+Student  received a grade below , so the grade will not be modified and the student's final grade is .
+"""
