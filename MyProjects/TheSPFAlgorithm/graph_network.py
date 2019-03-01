9aefff7068f020c4402a56657f7e68b6be1cd561
igor <i.matukin@gmail.com>
2019-03-01 04:45:54 +0300

    TheSPFAlgorithm

--- MyProjects/TheSPFAlgorithm/graph_network.py
+++ MyProjects/TheSPFAlgorithm/graph_network.py
@@ -2,31 +2,40 @@
 
 import networkx as nx
 import matplotlib.pyplot as plt
+import warnings
+
+warnings.filterwarnings(
+    action='ignore', module='matplotlib.figure', category=UserWarning,
+    message=('This figure includes Axes that are not compatible with tight_layout, '
+             'so results might be incorrect.')
+)
 
 # Ориентированный граф.
 G = nx.DiGraph()
 
 G.add_nodes_from(["Pennsauken", "London", "Frankfurt", "New York", "Washington D.C.", "Paris"])
 
-e=[("Pennsauken", "London", 315000),
-   ("Pennsauken", "Frankfurt", 315000),
-   ("Pennsauken", "New York", 26000),
-   ("London", "Pennsauken", 315000),
-   ("London", "Frankfurt", 22000),
-   ("Frankfurt", "London", 22000),
-   ("Frankfurt", "Pennsauken", 315000),
-   ("Frankfurt", "Washington D.C.", 250000),
-   ("Frankfurt", "Paris", 87000),
-   ("Paris", "Frankfurt", 87000),
-   ("Paris", "Washington D.C.", 600000),
-   ("Washington D.C.", "Paris", 600000),
-   ("Washington D.C.", "Frankfurt", 250000),
-   ("Washington D.C.", "New York", 22000),
-   ("New York", "Washington D.C.", 22000),
-   ("New York", "Pennsauken", 26000)]
+e=[ ("Pennsauken", "London", 315000),
+    ("Pennsauken", "Frankfurt", 315000),
+    ("Pennsauken", "New York", 26000),
+    ("London", "Pennsauken", 315000),
+    ("London", "Frankfurt", 22000),
+    ("Frankfurt", "London", 22000),
+    ("Frankfurt", "Pennsauken", 315000),
+    ("Frankfurt", "Washington D.C.", 250000),
+    ("Frankfurt", "Paris", 87000),
+    ("Paris", "Frankfurt", 87000),
+    ("Paris", "Washington D.C.", 600000),
+    ("Washington D.C.", "Paris", 600000),
+    ("Washington D.C.", "Frankfurt", 250000),
+    ("Washington D.C.", "New York", 22000),
+    ("New York", "Washington D.C.", 22000),
+    ("New York", "Pennsauken", 26000) ]
 G.add_weighted_edges_from(e)
 
 print(nx.dijkstra_path(G,"Pennsauken", "Paris"))
+print(nx.dijkstra_path_length(G, "Pennsauken", "Paris"))
+
 print(nx.bellman_ford_path(G, "Pennsauken", "Paris"))
 
 nx.draw(G,pos=nx.spectral_layout(G),nodecolor='r',edge_color='b',with_labels=True)
