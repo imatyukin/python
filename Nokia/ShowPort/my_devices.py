033db4cd9a558a200132e01123c622b5c1e9ad5f
Igor <i.matukin@gmail.com>
2020-01-31 11:56:05 +0300

    ShowPort

---
+++ Nokia/ShowPort/my_devices.py
@@ -0,0 +1,26 @@
+sasm_sros = {
+    'device_type': 'alcatel_sros_ssh',
+    'ip': '135.117.211.163',
+    'username': 'admin',
+    'password': 'admin',
+    'port': 22,
+#	'global_delay_factor': 2,
+}
+
+sr1e_sros = {
+    'device_type': 'alcatel_sros_ssh',
+    'ip': '135.117.211.161',
+    'username': 'admin',
+    'password': 'admin',
+    'port': 22,
+#	'global_delay_factor': 4,
+}
+
+sr_sros = {
+    'device_type': 'alcatel_sros_ssh',
+    'ip': '100.100.100.5',
+    'username': 'admin',
+    'password': 'admin',
+    'port': 22,
+#    'global_delay_factor': 2,
+}
