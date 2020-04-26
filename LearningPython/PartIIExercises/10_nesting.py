bbad8c56e6106137fbfe7e38e1896688522a5508
Igor <i.matukin@gmail.com>
2020-04-26 19:44:35 +0300

    Part II Exercises

---
+++ LearningPython/PartIIExercises/10_nesting.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+pers_inform = {'name': ('Igor', 'S.', 'Matyukin'), 'age': 40, 'job': 'Programmer', 'address': 'Saint-Petersburg',
+               'email': 'postmaster@domain.org', 'phone': '555-55-55'}
+print("name (first, middle, last):", pers_inform['name'][0], pers_inform['name'][1], pers_inform['name'][2])
+print("age:", pers_inform['age'])
+print("job:", pers_inform['job'])
+print("address:", pers_inform['address'])
+print("email address:", pers_inform['email'])
+print("phone number:", pers_inform['phone'])
