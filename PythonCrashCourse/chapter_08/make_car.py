56836b4f90fd5d5d93486ce79967b2d2c13527bb
Igor <i.matukin@gmail.com>
2020-04-26 20:33:23 +0300

    Python Crash Course

--- PythonCrashCourse/chapter_08/make_car.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-def make_car(manufacturer, model_name, **car_info):
-    car = {}
-    car['manufacturer'] = manufacturer
-    car['model_name'] = model_name
-    for key, value in car_info.items():
-        car[key] = value
-    return car
