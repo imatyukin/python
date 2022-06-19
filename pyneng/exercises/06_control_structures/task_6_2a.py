958099e7b272d998e77a01e5bd8cecf4ad8a869f
igor <i.matukin@gmail.com>
2022-06-19 23:22:24 +0300

    Python

--- pyneng/exercises/06_control_structures/task_6_2a.py
+++ pyneng/exercises/06_control_structures/task_6_2a.py
@@ -17,3 +17,21 @@
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 """
+
+import re
+ip_addr = input("Введите IP-адрес: ")
+match = re.match(r"^(\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3})$", ip_addr)
+ip_addr_s = ip_addr.split(".")
+if bool(match) is True and all((int(part) > -1 and int(part)) < 256 for part in ip_addr_s):
+    if ip_addr == '255.255.255.255':
+        print('local broadcast')
+    elif ip_addr == '0.0.0.0':
+        print('unassigned')
+    elif int(ip_addr.split(".")[0]) in range(1, 224):
+        print('unicast')
+    elif int(ip_addr.split(".")[0]) in range(224, 240):
+        print('multicast')
+    else:
+        print('unused')
+else:
+    print("Неправильный IP-адрес")
