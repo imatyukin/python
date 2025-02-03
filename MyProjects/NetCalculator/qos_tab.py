e50a722ace5762542a0162e5e85caba767ce2d00
imatyukin <igormatyukin@yandex.ru>
2025-02-03 16:15:56 +0300

    NetCalculator v2

--- MyProjects/NetCalculator/qos_tab.py
+++ MyProjects/NetCalculator/qos_tab.py
@@ -1,5 +1,5 @@
-from PyQt5.QtWidgets import QWidget, QVBoxLayout, QTableWidget, QTableWidgetItem, QHeaderView, QLineEdit, QHBoxLayout, QLabel
-from PyQt5.QtCore import Qt, QSortFilterProxyModel, QAbstractTableModel
+from PyQt5.QtWidgets import QWidget, QVBoxLayout, QTableWidget, QTableWidgetItem, QHeaderView, QLineEdit, QHBoxLayout
+from PyQt5.QtCore import Qt
 
 class QoSTab(QWidget):
     def __init__(self):
@@ -73,12 +73,20 @@ class QoSTab(QWidget):
         def mpls_exp_to_binary(exp_decimal):
             return format(exp_decimal, '03b')
 
+        def calculate_tos_decimal(ds_field_bin):
+            """
+            Рассчитывает полное значение ToS Decimal из DS Field Binary.
+            Первые 3 бита маппируются на TOS Precedence, остальные 5 битов заполняются нулями.
+            """
+            tos_precedence = int(ds_field_bin[:3], 2)  # Первые 3 бита DS Field
+            return tos_precedence << 5  # Сдвигаем на 5 битов влево
+
         # AF-классы (Assured Forwarding)
         for class_num in range(1, 5):  # Классы AF1–AF4
-            for drop_prob in range(1, 4):  # Уровни надежности (1–3)
-                dscp_name = f"AF{class_num}{drop_prob}"
-                ds_field_bin = f"{(class_num << 3 | drop_prob):06b}"  # Бинарное значение DS Field
-                ds_field_dec = int(ds_field_bin, 2)  # Десятичное значение DS Field
+            for drop_prob in range(0, 3):  # Уровни надежности (0-2)
+                dscp_name = f"AF{class_num}{drop_prob + 1}"
+                ds_field_dec = (class_num << 3) + (drop_prob << 1)  # **Correct Formula**
+                ds_field_bin = f"{ds_field_dec:06b}"
                 ds_field_hex = f"0x{ds_field_dec:02X}"  # Шестнадцатеричное значение DS Field
 
                 # TOS Precedence (первые 3 бита из DS Field)
@@ -86,6 +94,11 @@ class QoSTab(QWidget):
                 tos_hex = tos_to_hex(tos_precedence)
                 tos_dec = tos_precedence
                 tos_bin = tos_to_binary(tos_precedence)
+
+                # ToS Decimal (полное значение TOS)
+                tos_full_decimal = calculate_tos_decimal(ds_field_bin)
+                tos_full_bin = format(tos_full_decimal, '08b')
+                tos_full_hex = tos_to_hex(tos_full_decimal)
 
                 # ToS Name和服务 Class Name
                 tos_name = f"Precedence {class_num - 1}"
@@ -97,19 +110,26 @@ class QoSTab(QWidget):
 
                 qos_data.append((
                     dscp_name, ds_field_bin, str(ds_field_dec), ds_field_hex,
-                    str(tos_precedence), tos_hex, str(tos_dec), tos_bin,
+                    str(tos_precedence), tos_hex, str(tos_full_decimal), tos_full_bin,
                     tos_name, service_class_name, str(mpls_exp), mpls_exp_bin
                 ))
-
         # Expedited Forwarding (EF)
         ef_dscp = "EF"
         ef_ds_field_bin = "101110"
         ef_ds_field_dec = int(ef_ds_field_bin, 2)
         ef_ds_field_hex = f"0x{ef_ds_field_dec:02X}"
-        ef_tos_precedence = 5 * 8  # Приоритет TOS для EF
+
+        # TOS Precedence для EF (первые 3 бита DS Field)
+        ef_tos_precedence = (ef_ds_field_dec >> 5) & 0b111  # Первые 3 бита DS Field
         ef_tos_hex = tos_to_hex(ef_tos_precedence)
         ef_tos_dec = ef_tos_precedence
         ef_tos_bin = tos_to_binary(ef_tos_precedence)
+
+        # ToS Decimal (полное значение TOS)
+        ef_tos_full_decimal = calculate_tos_decimal(ef_ds_field_bin)
+        ef_tos_full_bin = format(ef_tos_full_decimal, '08b')
+        ef_tos_full_hex = tos_to_hex(ef_tos_full_decimal)
+
         ef_tos_name = "Expedited Forwarding"
         ef_service_class_name = "Expedited Forwarding"
         ef_mpls_exp = (ef_ds_field_dec >> 3) & 0b111  # Извлекаем MPLS EXP
@@ -117,7 +137,7 @@ class QoSTab(QWidget):
 
         qos_data.append((
             ef_dscp, ef_ds_field_bin, str(ef_ds_field_dec), ef_ds_field_hex,
-            str(ef_tos_precedence), ef_tos_hex, str(ef_tos_dec), ef_tos_bin,
+            str(ef_tos_precedence), ef_tos_hex, str(ef_tos_full_decimal), ef_tos_full_bin,
             ef_tos_name, ef_service_class_name, str(ef_mpls_exp), ef_mpls_exp_bin
         ))
 
@@ -126,10 +146,18 @@ class QoSTab(QWidget):
         be_ds_field_bin = "000000"
         be_ds_field_dec = int(be_ds_field_bin, 2)
         be_ds_field_hex = f"0x{be_ds_field_dec:02X}"
+
+        # TOS Precedence для BE
         be_tos_precedence = 0  # Приоритет TOS для BE
         be_tos_hex = tos_to_hex(be_tos_precedence)
         be_tos_dec = be_tos_precedence
         be_tos_bin = tos_to_binary(be_tos_precedence)
+
+        # ToS Decimal (полное значение TOS)
+        be_tos_full_decimal = calculate_tos_decimal(be_ds_field_bin)
+        be_tos_full_bin = format(be_tos_full_decimal, '08b')
+        be_tos_full_hex = tos_to_hex(be_tos_full_decimal)
+
         be_tos_name = "Best Effort"
         be_service_class_name = "Best Effort"
         be_mpls_exp = (be_ds_field_dec >> 3) & 0b111  # Извлекаем MPLS EXP
@@ -137,7 +165,7 @@ class QoSTab(QWidget):
 
         qos_data.append((
             be_dscp, be_ds_field_bin, str(be_ds_field_dec), be_ds_field_hex,
-            str(be_tos_precedence), be_tos_hex, str(be_tos_dec), be_tos_bin,
+            str(be_tos_precedence), be_tos_hex, str(be_tos_full_decimal), be_tos_full_bin,
             be_tos_name, be_service_class_name, str(be_mpls_exp), be_mpls_exp_bin
         ))
 
@@ -154,6 +182,11 @@ class QoSTab(QWidget):
             tos_dec = tos_precedence
             tos_bin = tos_to_binary(tos_precedence)
 
+            # ToS Decimal (полное значение TOS)
+            tos_full_decimal = calculate_tos_decimal(cs_ds_field_bin)
+            tos_full_bin = format(tos_full_decimal, '08b')
+            tos_full_hex = tos_to_hex(tos_full_decimal)
+
             # ToS Name和服务 Class Name
             tos_name = f"Class Selector {class_num}"
             service_class_name = "Class Selector"
@@ -164,7 +197,7 @@ class QoSTab(QWidget):
 
             qos_data.append((
                 cs_dscp, cs_ds_field_bin, str(cs_ds_field_dec), cs_ds_field_hex,
-                str(tos_precedence), tos_hex, str(tos_dec), tos_bin,
+                str(tos_precedence), tos_hex, str(tos_full_decimal), tos_full_bin,
                 tos_name, service_class_name, str(mpls_exp), mpls_exp_bin
             ))
 
